9f8bb4b95ba607dc7b7a2c74b9b29550dc17b9d3
jihoseo2006 <jihoseo2006@dsm.hs.kr>
2022-04-15 18:48:39 +0900

    my first unity project
    본인 유니티 첫 파일

---
+++ My project/Assets/move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public int speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
